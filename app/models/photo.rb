acc70fd404bd1239e784776844d2a852937de126
Sergii Mertsalov <student@epicodus.com>
2014-08-29 16:08:56 -0700

    add photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :photo, :styles => {
+  has_attached_file :photo, styles: {
     medium: '300x300>',
     small: '140x140>',
     thumb: '64x64!'
