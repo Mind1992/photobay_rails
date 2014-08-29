020e0c650803a6a7f6c6ad0b0237ae25b71f8a30
Sergii Mertsalov <student@epicodus.com>
2014-08-29 11:34:54 -0700

    validate email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
-class Contact < ActiveRecord::Base
+class User < ActiveRecord::Base
+has_secure_password
+validates :email, presence: true,
+                  uniqueness: { case_sensitive: false },
+                  format: {
+                    with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]+\Z/
+                  }
+
+validates :user_name, presence: true, uniqueness: { case_sensitive: false }
+before_save :downcase_userinfo
+
+private
+  def downcase_userinfo
+    self.user_name = self.user_name.downcase if self.user_name
+    self.email = self.email.downcase if self.email
+  end
+
 end
